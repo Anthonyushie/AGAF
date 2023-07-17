e874c3a77d909d8f257b0e709ede5922995029b7
Wutche <wutchechikaome75@gmail.com>
2023-07-17 12:13:20 +0100

    added to=top functionality

--- js/main.js
+++ js/main.js
@@ -1,50 +1,56 @@
+/** @format */
+
 //Loading effects
 var loader;
 
 function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-        } else {
-            loader.style.opacity = opacity;
-            window.setTimeout(function() {
-                loadNow(opacity - 0.01);
-            }, 30);
-        }
-    }
+	if (opacity <= 0) {
+		displayContent();
+	} else {
+		loader.style.opacity = opacity;
+		window.setTimeout(function () {
+			loadNow(opacity - 0.01);
+		}, 30);
+	}
+}
 
 function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
+	loader.style.display = "none";
+	document.getElementById("content").style.display = "block";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
+document.addEventListener("DOMContentLoaded", function () {
+	loader = document.getElementById("loader");
+	loadNow(1);
 });
 
-
-
 //code for toggle menu
-var navbar__list = document.getElementById("navbar__list")
-function showMenu(){
-    navbar__list.style.right = "0"
+var navbar__list = document.getElementById("navbar__list");
+function showMenu() {
+	navbar__list.style.right = "0";
 }
-function hideMenu(){
-    navbar__list.style.right = "-200px"
+function hideMenu() {
+	navbar__list.style.right = "-200px";
 }
-
 
 function copyToClipboard(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-  }
-  function copyEvent(id)
-              {
-                  var str = document.getElementById(id);
-                  window.getSelection().selectAllChildren(str);
-                  document.execCommand("Copy")
-              }
-  
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val($(element).text()).select();
+	document.execCommand("copy");
+	$temp.remove();
+}
+function copyEvent(id) {
+	var str = document.getElementById(id);
+	window.getSelection().selectAllChildren(str);
+	document.execCommand("Copy");
+}
+
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+	if (window.pageYOffset > 50) {
+		toTop.classList.add("active");
+	} else {
+		toTop.classList.remove("active");
+	}
+});
